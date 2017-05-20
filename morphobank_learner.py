587012555545ba0a59392fdc361ee7ee07caf2f3
Tim Zhang <TimothyHZhang@yahoo.com>
2017-05-20 18:38:50 -0400

    Add center markers and K command line argument

--- morphobank_learner.py
+++ morphobank_learner.py
@@ -4,9 +4,10 @@
 # ---
 # COMMAND LINE ARGUMENTS:
 # sys.argv[1] := a CSV list of paths to Morphobank feature files
+# sys.argv[2] := k, the number of clusters
 #
 # Example Usage: 
-# python3 morphobank_learner.py 'features.txt'
+# python3 morphobank_learner.py 'features.txt' '10'
 ####
 import sys
 import numpy as np
@@ -25,11 +26,16 @@ data = np.loadtxt(open(sys.argv[1]), delimiter=',')
 X = np.array(data)
 
 # Run learning algorithms
-k_means = KMeans(n_clusters = 2).fit_predict(X)
+kmeans = KMeans(n_clusters = int(sys.argv[2]), random_state = 7)
+clusters = kmeans.fit_predict(X)
+centroids = kmeans.cluster_centers_
 
 # Generate plot
 plt.xlabel('Jaccard Label Similarity')
 plt.ylabel('Jaccard State Similarity')
-plt.scatter(X[:, 0], X[:, 1], c = k_means)
+plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.scatter(X[:, 0], X[:, 1], c = clusters)
 
 plt.show()
